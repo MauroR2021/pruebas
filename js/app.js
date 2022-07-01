f340d057da4cf34b111078e907960bf1d54a1f10
Mauro <mauro@mauro.com>
2022-06-30 21:57:34 -0500

    nuevo cambio

--- js/app.js
+++ js/app.js
@@ -2,4 +2,8 @@ const elemento = document.querySelector('#pulsador');
 
 elemento.addEventListener("click", function(){
     alert("ME HAN PULSADO");
+
+    setTimeout(() => {
+        alert("VOLVÍ");
+    }, 5000);
 });
